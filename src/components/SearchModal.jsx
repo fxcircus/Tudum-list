bea31786d67dbcdbf740ed72a7a1235e70ebf5b0
Roy Daniel <roydanielr@gmail.com>
2022-11-08 20:54:11 -0500

    platform selection initial solution

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -12,12 +12,23 @@ export default function SearchModal ({ loadItems }) {
         type: "tv",
         url: ""
     })
-    const [ modalOpen, setModalOpen ] = useState (false)
+    const [ modalOpen, setModalOpen ] = useState (true)
     const [ img, setImg ] = useState ("")
     const [ icon, setIcon ] = useState("🔎")
     const [ inputValue, setInputValue ] = useState ("")
     const [ timer, setTimer ] = useState (null)
     const [ platform, setPlatform ] = useState ("")
+    const [ platformList, setPlatformList ] = useState ({
+        all: true,
+        netflix: true,
+        hulu: true,
+        apple: true,
+        peacock: true,
+        amazon: true,
+        hbomax: true,
+        disneyplus: true
+    })
+    const [ searchFilter, setSearchFilter ] = useState("&siteSearch=www.netflix.com&siteSearch=www.hulu.com&siteSearch=www.peacock.com&siteSearch=www.amazon.com&siteSearch=www.hbomax.com&siteSearch=www.disneyplus.com&siteSearch=www.tv.apple.com")
 
     const getUrl = async (lastEvt) => {
         setIcon("🌐")
@@ -76,6 +87,21 @@ export default function SearchModal ({ loadItems }) {
         loadItems()
     }
 
+    const changeSearchFilters = (val) => {
+        const newBoolVal = !platformList[val] 
+        let tmpCurFilter = searchFilter
+        const newFilter = val === "apple" ? `&siteSearch=www.tv.apple.com` : `&siteSearch=www.${val}.com`
+        if(newBoolVal) {
+            tmpCurFilter += newFilter
+        } else {
+            tmpCurFilter = tmpCurFilter.replace(newFilter, '')
+        }
+
+        setSearchFilter(tmpCurFilter)
+        console.log(searchFilter)
+        setPlatformList({...platformList, [val]: newBoolVal})
+    }
+
     return (
         <div>
             { modalOpen ? (
@@ -85,9 +111,12 @@ export default function SearchModal ({ loadItems }) {
                 >
             
               <div class="modal-content">
+                
                 <button type="button"
-                        className="modal-close-button"
-                        onClick={(e) => {closeModal()}}>&times;</button>
+                    className="modal-close-button"
+                    onClick={(e) => {closeModal()}}>&times;
+                </button>
+
                 <form className="add-new-item-div" onSubmit={handleSubmit}>
                     <input
                         type="text"
@@ -104,15 +133,54 @@ export default function SearchModal ({ loadItems }) {
                         <option value="tv">📺</option>
                         <option value="movie">🎬</option>
                     </select> */}
-
-                    {img ? (
-                        <div className="search-modal">
-                            <img className="thumbnail" src={img} alt="thumbnail" />
-                            <p>{platform}</p>
-                        </div>
-                    ) :
-                    ("")}
+                    
                 </form>
+                
+                <div className="platform-list">
+                    <span>Search on: </span>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.all ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("all")}} />All
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.netflix ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("netflix")}} />Netflix
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.hulu ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("hulu")}} />Hulu
+                        <span className="checkmark"></span>
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.apple ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("apple")}} />Apple TV+
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.peacock ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("peacock")}} />Peacock
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.amazon ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("amazon")}} />Amazon Prime
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox" checked={platformList.hbomax ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("hbomax")}} />HBO Max
+                    </label>
+                    <label className="platform-label">
+                        <input type="checkbox"checked={platformList.disneyplus ? "checked": null}
+                        onChange={(e) => {changeSearchFilters("disneyplus")}} />Disney
+                    </label>
+                </div>
+                
+                {img ? (
+                    <div className="returned-results">
+                        <img className="thumbnail" src={img} alt="thumbnail" />
+                        <p>{platform}</p>
+                    </div>
+                ) :
+                ("")}
+
               </div>
             
             </div>
